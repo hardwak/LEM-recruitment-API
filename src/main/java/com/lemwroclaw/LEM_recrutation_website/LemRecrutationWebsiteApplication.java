4f63df929a96a0d095f1803ebfdffc2968e81118
hardwak <ymhere2@gmail.com>
2024-09-26 15:18:18 +0200

    Initial Commit

---
+++ src/main/java/com/lemwroclaw/LEM_recrutation_website/LemRecrutationWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.lemwroclaw.LEM_recrutation_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemRecrutationWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemRecrutationWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemwroclaw/LEM_recrutation_website/LemRecrutationWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemwroclaw.LEM_recrutation_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemRecrutationWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
